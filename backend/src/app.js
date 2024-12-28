f2decaa2da848e4e7987bbd2ceae8419610fcee1
peedroca <phm1080@gmail.com>
2024-12-28 12:31:32 -0300

    Fixed docker-compose script

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,5 +17,5 @@ server.get('/', (req, res) => res.send('Working!'));
 
 server.use('/User', UserController);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT} ...`));
